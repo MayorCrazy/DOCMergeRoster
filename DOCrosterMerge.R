1377bda7394cd8d68cbedad27f661b550f03167c
Charles McCrea <cmccrea@pmhcc.org>
2021-06-24 13:22:32 -0400

    Changed formatting method and added styles.

--- DOCrosterMerge.R
+++ DOCrosterMerge.R
@@ -70,10 +70,67 @@ CCSRoster <-subset(SameClientsDOC,!(SameClientsDOC$Inmate.Number%in%CCSRoster$In
 
 TRoster<-merge.data.frame(NClientsDOC,SameClientsDOC,all = TRUE)
 TRoster<-TRoster[order(TRoster$Max.Date),]
+########### FORMATTING AND SUCH
+
+TRoster$DOB<-format(as.Date(TRoster$DOB),format="%m/%d/%Y")
+TRoster$Min.Date<-format(as.Date(TRoster$Min.Date),format="%m/%d/%Y")
+TRoster$Max.Date<-format(as.Date(TRoster$Max.Date),format="%m/%d/%Y")
+
+NClientsDOC$DOB<-format(as.Date(NClientsDOC$DOB),format="%m/%d/%Y")
+NClientsDOC$Min.Date<-format(as.Date(NClientsDOC$Min.Date),format="%m/%d/%Y")
+NClientsDOC$Max.Date<-format(as.Date(NClientsDOC$Max.Date),format="%m/%d/%Y")
+
+RClientsDOC$DOB<-format(as.Date(RClientsDOC$DOB),format="%m/%d/%Y")
+RClientsDOC$Min.Date<-format(as.Date(RClientsDOC$Min.Date),format="%m/%d/%Y")
+RClientsDOC$Max.Date<-format(as.Date(RClientsDOC$Max.Date),format="%m/%d/%Y")
+
+CCSRoster$DOB<-format(as.Date(CCSRoster$DOB),format="%m/%d/%Y")
+CCSRoster$Min.Date<-format(as.Date(CCSRoster$Min.Date),format="%m/%d/%Y")
+CCSRoster$Max.Date<-format(as.Date(CCSRoster$Max.Date),format="%m/%d/%Y")
+
+#Add workbook and worksheets
+wb<- createWorkbook("DOCrosters")
+addWorksheet(wb,"Roster",gridLines = FALSE)
+addWorksheet(wb,"New",gridLines = FALSE)
+addWorksheet(wb,"Removed",gridLines = FALSE)
+addWorksheet(wb,"Not in CCS",gridLines = FALSE)
+#Write data to worksheets
+writeData(wb, sheet = 1, TRoster, rowNames = FALSE)
+writeData(wb, sheet = 2, NClientsDOC, rowNames = FALSE)
+writeData(wb, sheet = 3, RClientsDOC, rowNames = FALSE)
+writeData(wb, sheet = 4, CCSRoster, rowNames = FALSE)
+
+#Create formatting styles
+allformat<-createStyle(halign = 'left',
+                       borderColour = getOption("openxlsx.borderColour", "black"),
+                       borderStyle = getOption("openxlsx.borderStyle", "thin"),
+                       border = 'TopBottomLeftRight'
+)
+centerformat<-createStyle(halign = 'center')
+headerformat<-createStyle(textDecoration ='Bold',fgFill ='#88c2a0')
+#Apply formatting styles
+addStyle(wb, sheet = 1, allformat, rows = 1:400, cols = 1:11, gridExpand = TRUE)
+addStyle(wb, sheet = 2, allformat, rows = 1:400, cols = 1:11, gridExpand = TRUE)
+addStyle(wb, sheet = 3, allformat, rows = 1:400, cols = 1:11, gridExpand = TRUE)
+addStyle(wb, sheet = 4, allformat, rows = 1:400, cols = 1:11, gridExpand = TRUE)
 
 
-sheets <- list("Roster" = TRoster, "New" = NClientsDOC,"Removed" = RClientsDOC,"Not in CCS" = CCSRoster) #assume sheet1 and sheet2 are data frames
-filepath<-("C:/Users/cmccrea/Box/BHJD/DOC/Rosters/")
-filename<-paste0(filepath,"DOC MH Roster Philadelphia ",format(Sys.Date(),"%Y-%b%d")," Unified.xlsx")
+addStyle(wb, sheet = 1, headerformat, rows = 1, cols = c(1:11), gridExpand = TRUE, stack = TRUE)
+addStyle(wb, sheet = 2, headerformat, rows = 1, cols = c(1:11), gridExpand = TRUE, stack = TRUE)
+addStyle(wb, sheet = 3, headerformat, rows = 1, cols = c(1:11), gridExpand = TRUE, stack = TRUE)
+addStyle(wb, sheet = 4, headerformat, rows = 1, cols = c(1:11), gridExpand = TRUE, stack = TRUE)
 
-write.xlsx(sheets,filename,colWidths="auto")
+addStyle(wb, sheet = 1, centerformat, rows = 1:5000, cols = c(4:6,8:11), gridExpand = TRUE, stack = TRUE)
+addStyle(wb, sheet = 2, centerformat, rows = 1:5000, cols = c(4:6,8:11), gridExpand = TRUE, stack = TRUE)
+addStyle(wb, sheet = 3, centerformat, rows = 1:5000, cols = c(4:6,8:11), gridExpand = TRUE, stack = TRUE)
+addStyle(wb, sheet = 4, centerformat, rows = 1:5000, cols = c(4:6,8:11), gridExpand = TRUE, stack = TRUE)
+
+setColWidths(wb,sheet =1, col=1:11,widths = 'auto')
+setColWidths(wb,sheet =2, col=1:11,widths = 'auto')
+setColWidths(wb,sheet =3, col=1:11,widths = 'auto')
+setColWidths(wb,sheet =4, col=1:11,widths = 'auto')
+
+FileName<-paste0("DOC MH Roster Philadelphia Unified TEST VERSION",format(Sys.Date(),"%Y-%b"),".xlsx")
+
+Path<-paste0("C:/Users/cmccrea/Box/BHJD/DOC/Rosters/",FileName)
+saveWorkbook(wb, Path, overwrite = TRUE)
